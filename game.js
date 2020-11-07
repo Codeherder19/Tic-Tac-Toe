d8179b6219bdd5014f7256fa652ed3551b7098aa
Brian Forbes <brianandrewf81@gmail.com>
2020-11-07 15:44:33 -0700

    Refactor functions so they are object methods and there is only one global variable in main.js

--- game.js
+++ game.js
@@ -1,7 +1,8 @@
 class Game {
-  constructor(player1, player2) {
+  constructor() {
     this.winConditions = [[0, 4, 8],[2, 4, 6],[0, 1, 2],[3, 4, 5],[6, 7, 8],[0, 3, 6],[1, 4, 7],[2, 5, 8]];
-    this.players = [player1, player2];
+    this.player1 = new Player('Player One', '☠️');
+    this.player2 = new Player('Player Two', '👹');
     this.playerTurn = 'Player One';
     this.gameBoard = [1, 2, 3, 4, 5, 6, 7, 8, 9];
   }
@@ -12,6 +13,24 @@ class Game {
     var startingBoard = [1, 2, 3, 4, 5, 6, 7, 8, 9];
     if (!this.gameboard.includes(startingBoard)) {
     return "This game is a draw";
-  }
-}
+    }
+  };
+
+  placePlayerToken(game, tokenLocation) {
+    if (game.playerTurn === this.player1.id) {
+    game.gameBoard.splice(tokenLocation, 1, this.player1.token);
+    this.alternatePlayerTurns();
+  } else if (game.playerTurn === this.player2.id) {
+    game.gameBoard.splice(tokenLocation, 1, this.player2.token);
+    this.alternatePlayerTurns();
+    }
+  };
+
+    alternatePlayerTurns() {
+    if (this.playerTurn === this.player1.id) {
+    this.playerTurn = 'Player Two';
+  } else if (this.playerTurn === this.player2.id) {
+    this.playerTurn = 'Player One';
+    }
+  };
 }
