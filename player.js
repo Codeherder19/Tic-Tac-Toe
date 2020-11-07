516b25f98f4c7ea1c50efdca1ae582b69b621b9e
Brian Forbes <brianandrewf81@gmail.com>
2020-11-07 11:24:20 -0700

    Create starting player and game classes

---
+++ player.js
@@ -0,0 +1,14 @@
+class Player {
+  constructor(playerNumber, token) {
+    this.token = token;
+    this.numOfWins = 0;
+    this.id = playerNumber;
+    this.tokensPlayed = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  }
+  saveToStorage() {
+
+  }
+  retrieveWinsFromStorage() {
+
+  }
+}
