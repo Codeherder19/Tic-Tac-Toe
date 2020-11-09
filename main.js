6401e763f888250fb4aee2a863e5d631e69fe392
Brian Forbes <brianandrewf81@gmail.com>
2020-11-08 21:39:45 -0700

    Experiment with code to select individual squares of gameboard. Nothing working yet. '

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
 var currentGame = new Game;
+var ticTacToeBoard = document.querySelectorAll('.game-square');
+
+
+
+// ticTacToeBoard.addEventListener('click', );
+window.addEventListener('load', persistPlayerWinsOnPageReload);
+
+
+
+
+function placeTokenOnGameBoard() {
+  
+}
+
+function persistPlayerWinsOnPageReload() {
+  currentGame.player1.retrieveWinsFromStorage();
+  currentGame.player2.retrieveWinsFromStorage();
+}
